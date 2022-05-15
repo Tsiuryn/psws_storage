116314b2433a85af81b1110c8778a5e331739ffd
Alex Tsiuryn <>
2022-05-15 20:20:04 +0300

    added AppLocalization

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'router/app_router.dart';
 
 class MyApp extends StatelessWidget {
@@ -13,6 +15,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routerDelegate: appRouter.delegate(),
       routeInformationParser: appRouter.defaultRouteParser(),
+      localizationsDelegates: const[
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+        Locale('ru', ''), // Spanish, no country code
+      ],
     );
   }
 }
